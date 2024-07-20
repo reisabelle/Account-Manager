c608d618dca7e0f3f66a5a3a7a5088ff8fe89e18
Reina Isabelle Abejuro <166028125+reisabelle@users.noreply.github.com>
2024-07-20 15:50:38 +0800

    Adding ImagePicker

--- app/src/main/java/com/example/accountmanagement/register.kt
+++ app/src/main/java/com/example/accountmanagement/register.kt
@@ -15,16 +15,34 @@ import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import androidx.activity.enableEdgeToEdge
 
+import com.github.dhaval2404.imagepicker.ImagePicker
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
 class register : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
     private lateinit var database: DatabaseReference
+
+    private lateinit var button: FloatingActionButton
+    private lateinit var imageview: ImageView
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_register)
+
+         imageview = findViewById(R.id.profile)
+        button = findViewById(R.id.camera)
+
+        button.setBackgroundColor(Color.TRANSPARENT)
+        button.setOnClickListener {
+            ImagePicker.with(this)
+                .crop()	    			//Crop image(Optional), Check Customization for more option
+                .compress(1024)			//Final image size will be less than 1 MB(Optional)
+                .maxResultSize(1080, 1080)	//Final image resolution will be less than 1080 x 1080(Optional)
+                .start()
+        }
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -50,9 +68,13 @@ class register : AppCompatActivity() {
             if (email.isEmpty() || password.isEmpty() || username.isEmpty() || phone.isEmpty()) {
                 Toast.makeText(this, "Please fill all fields", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
+            } else {
+                registerUser(email, password, username, phone)
+                usernameEditText.text.clear()
+                emailEditText.text.clear()
+                phoneEditText.text.clear()
+                passwordEditText.text.clear()
             }
-
-            registerUser(email, password, username, phone)
         }
 
         val backbtn2: ImageView = findViewById(R.id.backbtn2)
@@ -62,6 +84,12 @@ class register : AppCompatActivity() {
         }
     }
 
+     @Deprecated("This method has been deprecated in favor of using the Activity Result API\n      which brings increased type safety via an {@link ActivityResultContract} and the prebuilt\n      contracts for common intents available in\n      {@link androidx.activity.result.contract.ActivityResultContracts}, provides hooks for\n      testing, and allow receiving results in separate, testable classes independent from your\n      activity. Use\n      {@link #registerForActivityResult(ActivityResultContract, ActivityResultCallback)}\n      with the appropriate {@link ActivityResultContract} and handling the result in the\n      {@link ActivityResultCallback#onActivityResult(Object) callback}.")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        imageview.setImageURI(data?. data)
+    }
+    
     private fun registerUser(email: String, password: String, username: String, phone: String) {
         auth.createUserWithEmailAndPassword(email, password)
             .addOnCompleteListener(this) { task ->
